7d9b1ffde24d2a0bf67a3ef1f7097ef9988a2a25
Kpk48 <praveenkumark2006@gmail.com>
2026-06-04 11:28:01 +0530

    Fix env vars

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,15 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function getSupabaseBrowser() {
-  console.log("BROWSER URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-  console.log("BROWSER ANON:", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  console.log(
+    "CLIENT URL:",
+    process.env.NEXT_PUBLIC_SUPABASE_URL
+  );
+
+  console.log(
+    "CLIENT KEY EXISTS:",
+    !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
 
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
