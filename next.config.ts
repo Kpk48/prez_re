ee247aae8407d1c33dc3fdb8cbd03bcc6a6b0879
Kpk48 <praveenkumark2006@gmail.com>
2026-06-04 13:05:08 +0530

    Fx R and Supabase env configuration

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,19 @@
-console.log("BUILD URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL:
+      "https://ctnnjaowvsoiwlgetbyn.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0bm5qYW93dnNvaXdsZ2V0YnluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NTgyMDQsImV4cCI6MjA5NjAzNDIwNH0.sCOrsKGAAYnctAYkKIdMLrqA7UhSwONRgd0vrYF7OGI",
+  },
   reactCompiler: true,
-  output: 'standalone',
+  output: "standalone",
   experimental: {
     serverActions: {
-      bodySizeLimit: '4mb'
-    }
-  }
+      bodySizeLimit: "4mb",
+    },
+  },
 };
 
 export default nextConfig;
