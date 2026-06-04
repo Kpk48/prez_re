2d125f42164d03939807de8fa6a28a5f8cc3c871
Kpk48 <praveenkumark2006@gmail.com>
2026-06-04 10:54:54 +0530

    v3

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function getSupabaseBrowser() {
+  console.log("BROWSER URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("BROWSER ANON:", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
